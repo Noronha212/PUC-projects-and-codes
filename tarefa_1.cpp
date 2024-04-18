b2ea200b3b985ccd4df73c1d801129fb05b377d6
Noronha212 <167235730+Noronha212@users.noreply.github.com>
2024-04-18 16:54:10 -0300

    updete else

--- tarefa_1.cpp
+++ tarefa_1.cpp
@@ -3,18 +3,27 @@
 #define PI 3.14
 int main(void)
 {
+	int i;
 	float raio, area;
 	
-	printf("este programa calcula a area de qualquer Area\n");
-	
-	printf("Qual e o raio da sua circunferencia: ");
+	printf("---------este programa calcula a area de qualquer Area----------\n");
+		
+ 	printf("Qual e o raio da sua circunferencia: ");
 	scanf("%f", &raio);
-	if( raio > 0){
-	   area = PI * (raio*raio);
+	  
+	  
+	    if( raio > 0){
+	    	
+	      area = PI * (raio*raio);
+	      printf("a area da sua circunferencia e igual a %.2f\n", area);
+        
+		}else{
+			
+    	 
+		  printf("valor invalido, tente novamente\n");
+		  printf("fim do ptograma, obrigado\n");
+        }  
 	
-	   printf("a area da sua circunferencia e igual a %.2f\n", area);
-	   printf("fim do ptograma, obrigado\n");
-    }
 	system("pause");
 	return 0;
 }
