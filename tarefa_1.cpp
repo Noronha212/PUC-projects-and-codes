aa671e480ec83cb23f788c0c386c0521a6084aa2
Noronha212 <167235730+Noronha212@users.noreply.github.com>
2024-04-23 14:52:20 -0300

    updete switch case

--- tarefa_1.cpp
+++ tarefa_1.cpp
@@ -3,25 +3,53 @@
 #define PI 3.14
 int main(void)
 {
-	int i;
-	float raio, area;
-	
-	printf("---------este programa calcula a area de qualquer Area----------\n");
+	float raio, area, a, b, soma;
+	int auxiliar;
 	
 	do{
 		
- 	  printf("Qual e o raio da sua circunferencia: ");
-	  scanf("%f", &raio);
-	  
-	  
-	    if( raio >= 0){
+ 	    printf("escolha uma das tres opcoes abaixo: \n");
+		printf("1 - cacular area da circunferencia\n");
+		printf("2 - somar dois numeros qualquer\n");
+		printf("3 - sair\n");
+		scanf("%d", &auxiliar);
+		
+		switch(auxiliar)
+		{
+		case 1:
+		    printf("informe o valor do raio: ");
+			scanf("%f", &raio);
+			
+			if( raio >= 0){
 	    	
-	      area = PI * (raio*raio);
-	      printf("a area da sua circunferencia e igual a %.2f\n", area);
-        
+	            area = PI * (raio*raio);
+	            printf("a area da sua circunferencia e igual a %.2f\n\n", area);
+            
+		    }else{
+		       
+		        printf("valor invalido\n\n");
+		    
+		    }
+		    break;
+		
+		case 2:
+			printf("quais sao os dois valores que voce quer somar(separados por espaco): ");
+		    scanf("%f %f", &a, &b);
+		    
+		    soma = a + b;
+		    
+		    printf("sua soma foi igual a %.1f\n\n", soma);
+		    break;
+		    
+		case 3:
+			raio = -1;
+			break;
+			
+		default:
+			break;
 		}
 			
-    } while(raio >= 0);
+    } while(raio > 0);
 	 
 	
 	printf("fim do ptograma, obrigado\n");
